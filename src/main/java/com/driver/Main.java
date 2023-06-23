df3c8840ef17d78436881b5c35effe2d3b947dbe
vasu <pentapativasu3333@gmail.com>
2023-06-23 22:36:53 +0530

    added second time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,30 @@
 package com.driver;
 
-import java.util.Scanner;
-
 public class Main {
-    public class Product{
+    public static void main(String[] args) {
+        Product p = new Product();
 
-        public int product(int x, int y){
-          return x*y;
+        int result1 = p.product(2, 3);
+        System.out.println("Result 1: " + result1);
+
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Result 2: " + result2);
+
+        double result3 = p.product(2.5, 3.5);
+        System.out.println("Result 3: " + result3);
+    }
+
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
         }
-        public int product(int x,int y,int z){
-          return x*y*z;
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
         }
+
         public double product(double x, double y) {
-            return x*y;
+            return x * y;
         }
     }
-public void main(String[] args){
-    Product p=new Product();
-    int result1=p.product(2,3);
-    System.out.println(result1);
-    int result2=p.product(2,3,4);
-    System.out.println(result2);
-    double result3=p.product(2.5,3.5);
-    System.out.println(result3);
-}
 }
