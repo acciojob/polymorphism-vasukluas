ed16285acaecc8a42782ef0a9d4d72943e72cc77
vasu <pentapativasu3333@gmail.com>
2023-06-23 22:30:45 +0530

    added first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public class Product{
+
+        public int product(int x, int y){
+          return x*y;
+        }
+        public int product(int x,int y,int z){
+          return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+public void main(String[] args){
+    Product p=new Product();
+    int result1=p.product(2,3);
+    System.out.println(result1);
+    int result2=p.product(2,3,4);
+    System.out.println(result2);
+    double result3=p.product(2.5,3.5);
+    System.out.println(result3);
+}
 }
